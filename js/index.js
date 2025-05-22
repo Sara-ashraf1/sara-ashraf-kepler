8b0586bb359f8f45a6ea15d43d41e63c430ec175
Sara-ashraf1 <sash786001@gmail.com>
2025-05-22 18:00:08 -0400

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -59,3 +59,26 @@ messageForm.addEventListener("submit", (event) => {
 
     event.target.reset();
 } )
+
+fetch('https://api.github.com/users/Sara-ashraf1/repos')
+.then(response => {
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.json()
+})
+.then(data => {
+    const repositories = data;
+    console.log(repositories);
+
+    const projectSection = document.getElementById('Projects');
+    const projectList = projectSection.querySelector('ul');
+
+    for (let i = 0; i < repositories.length; i++) {
+        const project = document.createElement('li');
+        project.innerText = repositories[i].name;
+        projectList.appendChild(project);
+}
+})
+.catch(error => console.error(error));
+
